95ea26a4e40a47a24a9e78aae6dd1b59322ce473
David Oliveira Silva <daviddeboaa@gmail.com>
2025-09-14 18:56:15 -0300

    Commit inicial com arquivos do projeto Vite!

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Paginas
+import Dashboard from './pages/Dashboard/Dashboard'
+import Map from './pages/Map/Map'
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Dashboard/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
