aa33be9d0c1b062b4aecb30999284535a145bdaf
David <daviddeboaa@gmail.com>
2025-09-18 01:53:01 -0300

    Adiciona funcionalidade de visualização de áreas no mapa com círculos e popups; calcula total de pessoas por área e ajusta estilos de popup.

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,15 +1,42 @@
-import { MapContainer, TileLayer, ImageOverlay, Polygon, useMap, useMapEvents} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Circle} from 'react-leaflet';
+import { createRoot } from 'react-dom/client';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import SideMenu from '../../components/SideMenu/SideMenu'
 import { DataContext } from '../../contexts/Contexts'
+import Icon from '../../components/Icon/Icon'
 import styles from './Map.module.css'
 
-import { useState, useContext } from 'react'
+import { useState, useContext, useMemo } from 'react'
 
 
 function Map() {
 
   const { data, dataInfo } = useContext(DataContext); // Dados dos anexos vindos do contexto
+
+  // Calcula o total de pessoas por área
+  const totalPeoplePerArea = useMemo(() => {
+    return (data ?? []).reduce((acc, item) => {
+      if (!acc[item.area]) acc[item.area] = 0; // Inicializa a contagem se a área ainda não estiver no acumulador
+      acc[item.area] += item.quantidade;
+      return acc;
+    }, {}); 
+  }, [data]);
+
+  // Obtem as coordenadas das areas
+  const areaCoordinates = useMemo(() => {
+    let areaCoords = {};
+    (dataInfo.areas ?? []).forEach((item) => {
+      areaCoords[item.nome] = [item.latitude, item.longitude];
+    });
+    return areaCoords;
+  }, [dataInfo]);
+
+  // Gerar cores para cada área
+  const getAreaColor = (index) => {
+      const colors = [ "#3511d4ff", "#4763ffff", "#7492FC", "#00E0B4", "#feda6cff"];
+      return colors[index % colors.length];
+  };
 
   return (
     <div className={styles.body}>
@@ -24,8 +51,8 @@ function Map() {
         </header>
 
         <MapContainer 
-          center={[-12.962195, -38.463982]}
-          zoom={15}
+          center={[-12.854396, -38.454726]}
+          zoom={18}
           style={{ height: '100%', width: '100%' }}
           zoomControl={false}
           attributionControl={false}
@@ -37,6 +64,42 @@ function Map() {
             url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
             attribution='&copy; <a href="https://carto.com/">carto.com</a> contributors'
           />
+
+          {/* Renderizar markers para cada área */}
+          {Object.entries(areaCoordinates).map(([areaName, coordinates], index) => {
+            const totalPeople = totalPeoplePerArea[areaName] || 0;
+            const areaColor = getAreaColor(index);
+            
+            return (
+              <Circle
+                  key={areaName}
+                  center={coordinates}
+                  radius={20}
+                  pathOptions={{
+                    color: areaColor,
+                    fillColor: areaColor,
+                    fillOpacity: 0.6,
+                    weight: 3
+                  }}
+                >
+                <Popup>
+                  <div className={styles.popup}>
+                    <span className={styles.popupAreaName} style={{ color: areaColor }}>{areaName}</span>
+                    <div className={styles.popupInfoContainer}>
+
+                      <div className={styles.popupIconContainer}>
+                        <Icon icon="fi fi-sr-users-alt" className={styles.popupIcon} />
+                        <span className={styles.popupInfoText}>{totalPeople}</span>
+                      </div>
+
+                    </div>
+                  </div>
+                  
+                </Popup>
+              </Circle>
+            );
+          })}
+
         </MapContainer>
 
       </div>
